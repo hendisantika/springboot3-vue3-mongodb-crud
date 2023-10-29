43d8942ba94630794220937ea4e3f98288981d47
hendisantika <hendisantika@yahoo.co.id>
2023-10-29 08:20:04 +0700

    #18 - Add User.vue in frontend ✏️📗:octocat:⬆

--- frontend/components/User.vue
+++ frontend/components/User.vue
@@ -30,3 +30,68 @@
   </div>
 </template>
 <script>
+import UserDataService from "../service/UserDataService";
+
+export default {
+  name: "User",
+  data() {
+    return {
+      firstName: "",
+      lastName: "",
+      emailId: "",
+      errors: [],
+    };
+  },
+  computed: {
+    id() {
+      return this.$route.params.id;
+    },
+  },
+  methods: {
+    refreshUserDetails() {
+      UserDataService.retrieveUser(this.id).then((res) => {
+        this.firstName = res.data.firstName;
+        this.lastName = res.data.lastName;
+        this.emailId = res.data.emailId;
+      });
+    },
+    validateAndSubmit(e) {
+      e.preventDefault();
+      this.errors = [];
+      if (!this.firstName) {
+        this.errors.push("Enter valid values");
+      } else if (this.firstName.length < 5) {
+        this.errors.push("Enter atleast 5 characters in First Name");
+      }
+      if (!this.lastName) {
+        this.errors.push("Enter valid values");
+      } else if (this.lastName.length < 5) {
+        this.errors.push("Enter atleast 5 characters in Last Name");
+      }
+      if (this.errors.length === 0) {
+        if (this.id == -1) {
+          UserDataService.createUser({
+            firstName: this.firstName,
+            lastName: this.lastName,
+            emailId: this.emailId,
+          }).then(() => {
+            this.$router.push("/");
+          });
+        } else {
+          UserDataService.updateUser(this.id, {
+            id: this.id,
+            firstName: this.firstName,
+            lastName: this.lastName,
+            emailId: this.emailId,
+          }).then(() => {
+            this.$router.push("/");
+          });
+        }
+      }
+    },
+  },
+  created() {
+    this.refreshUserDetails();
+  },
+};
+</script>
